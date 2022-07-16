8d2a8ce0ff7e98803350acfd23124e606d88c23c
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-07-16 07:40:31 +0200

    Added template config

---
+++ duplicateAndDeletedFileTracker/config_TEMPLATE.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+
+class config:
+    rootPath: Path = Path('\\folder\\where\\archive\\to\\track\\is\\located')
+    connect: dict = {
+        'dbname': 'NAME',
+        'user': 'USERNAME',
+        'password': 'PASSWORD'
+    }
